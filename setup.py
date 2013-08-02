80b731a5ec8921fda373253d7d51fde3dfd6ff60
ksahlin <kr_sahlin@hotmail.com>
2013-08-02 18:13:41 +0200

    fixed pysam version to 0.7 in setup.py.

--- setup.py
+++ setup.py
@@ -9,14 +9,11 @@ setup(
         "Development Status :: 4 - Beta",
         "Environment :: Console",
         "Intended Audience :: Science/Research",
-        #"License :: GNU General Public License v3 or later (GPLv3+)",
         "Natural Language :: English",
         "Operating System :: POSIX :: Linux",
         "Programming Language :: Python",
         "Topic :: Scientific/Engineering :: Bio-Informatics"
       ],
-    #py_modules=['BESST.py'],
-    #packages=['BESST',],
     scripts=['runBESST', 'docs/INSTALL.txt', 'docs/MANUAL.txt'],
     description='Scaffolder for genomic assemblies.',
     author='Kristoffer Sahlin',
@@ -24,9 +21,9 @@ setup(
     url='https://github.com/ksahlin/BESST',
     license='GPLv3',
     long_description=open(os.path.join(os.getcwdu(), 'README.md')).read(),
-    #requires=['pysam (>=0.6)','networkx (>=1.4)'],
-    install_requires=['pysam==0.6',
+
+    install_requires=['pysam==0.7',
                       'networkx>=1.4',
                       'mathstats'],
-    #platforms=['Unix', 'Linux', 'Mac OS']
+
 )
