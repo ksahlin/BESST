389c244966374aaa2825095d3a753aa30de306ee
ksahlin <kr_sahlin@hotmail.com>
2013-09-02 07:05:05 +0200

    KS transform- update setup.py and Versions.txt

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='BESST',
-    version='1.0.4.2',
+    version='1.0.4.3',
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
     classifiers=[
         "Development Status :: 4 - Beta",
