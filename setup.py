ed0a422eb2a151a18dc37230c2460580a0f650e0
ksahlin <kr_sahlin@hotmail.com>
2014-09-02 10:03:55 +0300

    Removed INSTALL.txt and MAUNAL.txt from setuptools/scripts because they are not python scripts.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='BESST',
-    version='1.2',
+    version='1.2.1',
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
     classifiers=[
         "Development Status :: 4 - Beta",
@@ -15,7 +15,7 @@ setup(
         "Programming Language :: Python",
         "Topic :: Scientific/Engineering :: Bio-Informatics"
       ],
-    scripts=['runBESST', 'docs/INSTALL.txt', 'docs/MANUAL.txt', 'scripts/reads_to_ctg_map.py'],
+    scripts=['runBESST', 'scripts/reads_to_ctg_map.py'],
     description='Scaffolder for genomic assemblies.',
     author='Kristoffer Sahlin',
     author_email='kristoffer.sahlin@scilifelab.se',
