01aff063e32f6916a9b3b11ddd739c8984fdb55d
ksahlin <krsahlin@gmail.com>
2015-05-26 15:53:24 +0200

    bumped up verson number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='BESST',
-    version='1.3.7',
+    version='1.3.8',
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
     classifiers=[
         "Development Status :: 4 - Beta",
