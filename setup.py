e2aa3abb6ef759f8c08ec60cfe7e0060cbaa0736
Kristoffer Sahlin <kxs624@psu.edu>
2016-07-12 16:25:35 -0400

    require mathstats 0.2.5 -- a new version which (hopefully) fixes the bug reported in https://github.com/ksahlin/BESST/issues/33

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='BESST',
-    version='2.2.4',
+    version='2.2.5',
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
     classifiers=[
         "Development Status :: 4 - Beta",
@@ -24,6 +24,6 @@ setup(
     long_description=open(os.path.join(os.getcwdu(), 'README.md')).read(),
     install_requires=['pysam>=0.7',
                       'networkx>=1.9',
-                      'mathstats>=0.2.4',
+                      'mathstats>=0.2.5',
                       'scipy>=0.9'],
 )
